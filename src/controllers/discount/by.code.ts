1a8d721e80d90f36046001295a8e665ad755be94
Eric Haney <erichaney144@gmail.com>
2023-01-03 20:23:47 -0800

    Add isValidForUse attribute

--- src/controllers/discount/by.code.ts
+++ src/controllers/discount/by.code.ts
@@ -1,9 +1,15 @@
 import { Discount } from 'entities/Discount'
+import { EveryNthOrderDiscount } from 'entities/EveryNthOrderDiscount'
 import { Request, Response } from 'express'
 
 export async function getDiscountByCodeController(
 	req: Request,
 	res: Response
 ): Promise<void> {
-	res.json(Discount.findByCode(req.params.code))
+	const discount = Discount.findByCode(req.params.code)
+	if (discount && discount instanceof EveryNthOrderDiscount) {
+		res.json({ ...discount, isValidForUse: discount.isEligible() })
+		return
+	}
+	res.json(discount)
 }
